0332a52df1fbc0eb620ee7f4e75bd1ee9a9125eb
Filipe Zatta <filipe.fbz@gmail.com>
2023-06-16 12:19:23 -0300

    add bats para rodar os arquivos

--- desktop_quimica/src/main.js
+++ desktop_quimica/src/main.js
@@ -9,8 +9,8 @@ if (require('electron-squirrel-startup')) {
 const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1280,
+    height: 720,
     webPreferences: {
       preload: MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY,
     },
@@ -20,7 +20,7 @@ const createWindow = () => {
   mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  //mainWindow.webContents.openDevTools();
 };
 
 // This method will be called when Electron has finished
--- desktop_quimica/src/renderer.js
+++ desktop_quimica/src/renderer.js
@@ -26,6 +26,7 @@
  * ```
  */
 
-import './index.css';
+import './main.css';
+import './tabela.css';
 
 console.log('👋 This message is being logged by "renderer.js", included via webpack');
